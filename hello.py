d1bba5b7a3a05ce2a9532bc68b0a8f8e7925a586
Gohil Vijay <gohilvijay53949@gmail.com>
2026-08-06 08:21:56 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-# Ask the user for an integer (whole number)
-number = int(input("Enter a whole number: "))
-
-# Check if the remainder when dividing by 2 is 0
-if number % 2 == 0:
-    print("This is an EVEN number.")
-else:
-    print("This is an ODD number.")
